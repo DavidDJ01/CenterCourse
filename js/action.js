09821b8db4856fd52791b1cb116b686dc0b6770b
daodc <chidao1080@gmail.com>
2025-12-11 00:42:22 +0700

    add form register

---
+++ js/action.js
@@ -0,0 +1,6 @@
+
+document.querySelector("#btnIcon").addEventListener("click", function(){
+        var state = document.querySelector(".sub-nav")
+        state.classList.toggle("active")
+        console.log("Oke")
+})
