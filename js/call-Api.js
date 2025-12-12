6eeae1fb947f594191fbf7b8800e4150a0521665
daodc <chidao1080@gmail.com>
2025-12-13 00:00:20 +0700

    add toast notification

--- js/call-Api.js
+++ js/call-Api.js
@@ -3,6 +3,26 @@ if (window.jQuery) {
 }
 
 $(document).ready(function () {
+
+
+    function createToast(type, icon, title, text) {
+        let notifications = document.querySelector('.notifications');
+        let newToast = document.createElement('div');
+        newToast.innerHTML = `
+            <div class="toast ${type}">
+                <i class="${icon}"></i>
+                <div class="content">
+                    <div class="title">${title}</div>
+                    <span>${text}</span>
+                </div>
+                <i class="fa-solid fa-xmark" onclick="(this.parentElement).remove()"></i>
+            </div>`;
+        notifications.appendChild(newToast);
+        newToast.timeOut = setTimeout(
+            () => newToast.remove(), 10000
+        )
+    }
+
     var guest = {
 
     }
@@ -19,6 +39,11 @@ $(document).ready(function () {
 
 
     $("#btnRegister").click(function () {
+        let type = 'success';
+        let icon = 'fa-solid fa-circle-check';
+        let title = 'Đăng Ký Thành Công';
+        let text = 'Hãy chờ thông tin phản hồi trong gmail nhé!';
+
         getDataInput();
         guest.gmail = gmail
         guest.hoTen = hoTen
@@ -27,31 +52,36 @@ $(document).ready(function () {
         if (gmail.trim() === "") {
             $("#gmailError").text("* Không được để trống")
         }
-        if (hoTen.trim() === "") {
+        else if (hoTen.trim() === "") {
             $("#hoTenError").text("* Không được để trống")
         }
-        if (sdt.trim() === "") {
+        else if (sdt.trim() === "") {
             $("#sdtError").text("* Không được để trống")
+        }
+        else if (document.cookie.includes(`registeredEmail=${guest.gmail}`)) {
+            let type = 'error';
+            let icon = 'fa-solid fa-circle-exclamation';
+            let title = 'Lỗi';
+            let text = 'Email này đã được đăng ký';
+            createToast(type, icon, title, text);
         }
         else {
             $("#gmailError").text("")
             $("#hoTenError").text("")
             $("#sdtError").text("")
+            document.cookie = `registeredEmail=${guest.gmail}; max-age=31536000`; // 1 năm
             $.ajax({
                 type: "post",
                 url: "https://nodemailer-api-tu7e.vercel.app/api/sendmailer",
                 data: guest,
                 success: function (response) {
                     console.log(response)
-                 //   console.log(`----info----- \n Gmail : ${response.email} \n Họ Và Tên: ${response.hoTen} \n Số điện thoại: ${response.sdt}`)
-                }
+                    createToast(type, icon, title, text);
+                    //   console.log(`----info----- \n Gmail : ${response.email} \n Họ Và Tên: ${response.hoTen} \n Số điện thoại: ${response.sdt}`)
+                },
             });
-          
-           // console.log(`gmail : ${guest.gmail} tên : ${guest.hoTen}, sdt: ${guest.sdt}`)
         }
-
-
-
+        // console.log(`gmail : ${guest.gmail} tên : ${guest.hoTen}, sdt: ${guest.sdt}`)
     })
 
 
