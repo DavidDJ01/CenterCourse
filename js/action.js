333d1839e394360c1969c2e610a16e911e6a1f04
daodc <chidao1080@gmail.com>
2025-12-13 23:00:15 +0700

    fix animation slide

--- js/action.js
+++ js/action.js
@@ -4,6 +4,22 @@ document.querySelector("#btnIcon").addEventListener("click", function () {
         state.classList.toggle("active")
         console.log("Oke")
 
-       
+
 })
+
+const track = document.querySelector(".slide-track");
+const radios = document.querySelectorAll(".slider-nav input");
+
+radios.forEach((radio, index) => {
+        radio.addEventListener("change", () => {
+                track.style.animationPlayState = "paused";
+                track.style.marginLeft = `-${100 * index}%`; // mỗi click trượt 1 ảnh
+                radios[index].checked = true;
+
+                // bật lại animation sau 2 giây
+                setTimeout(() => {
+                        track.style.animationPlayState = "running";
+                }, 2000);
+        });
+});
 
