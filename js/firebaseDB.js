a6cd6e73a624d107548591d7e96a5b2d320bf6e8
daodc <chidao1080@gmail.com>
2025-12-17 14:37:30 +0700

    add js firebase

--- js/firebaseDB.js
+++ js/firebaseDB.js
@@ -11,7 +11,8 @@ import {
   setDoc,
   doc,
   deleteDoc,
-  serverTimestamp
+  serverTimestamp,
+  onSnapshot
 } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
 import { getAuth, signInAnonymously } from
   "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
@@ -40,7 +41,7 @@ $(document).ready(async function () {
   var db = getFirestore(app)
 
   await signInAnonymously(auth)
-  
+
   const ManagerBehaviosUser = collection(db, "UserInfoLoadWeb_Db")
   var userId = getCookie("userid");
 
@@ -67,19 +68,19 @@ $(document).ready(async function () {
   async function addData(user) {
     const isId = await getDocId(userId);
     if (!isId) {
-      await setDoc(doc(ManagerBehaviosUser, userId), user, {merge : true}).then(() => {
+      await setDoc(doc(ManagerBehaviosUser, userId), user, { merge: true }).then(() => {
         console.warn("success")
       })
     } else {
       userId = userId + `_${Date.now()}`;
-      await setDoc(doc(ManagerBehaviosUser, userId), user,{merge : true} ).then(() => {
+      await setDoc(doc(ManagerBehaviosUser, userId), user, { merge: true }).then(() => {
         console.warn("success")
       })
     }
   }
 
-  async function UpdateData(userUP) {
-    await setDoc(doc(ManagerBehaviosUser, "user_1765904148203_428"), userUP, { merge: true })
+  async function UpdateData(userid, data) {
+    await setDoc(doc(ManagerBehaviosUser, userid), { TimePauseKey: data.timePause, status: data.status }, { merge: true })
       .then(() => {
         console.warn("success")
       })
@@ -90,16 +91,16 @@ $(document).ready(async function () {
       console.warn('success')
     })
   }
-//get cookie
-function getCookie(name) {
+  //get cookie
+  function getCookie(name) {
     const value = `; ${document.cookie}`;
     const parts = value.split(`; ${name}=`);
 
     if (parts.length === 2) {
-        return parts.pop().split(';').shift();
+      return parts.pop().split(';').shift();
     }
     return null;
-}
+  }
 
   // Set cookie
   function setCookie(name, value, days) {
@@ -126,96 +127,101 @@ function getCookie(name) {
   else {
     User.TypeDevice = "Computer"
   }
-  
+
   let clickCount = 0;
 
-  $(document).click(function (e) { 
-     clickCount++  
+  $(document).click(function (e) {
+    clickCount++
   });
- 
-  var now = new Date();
-  User.DateComeToWeb = `${now.getDate()}/${now.getMonth()+1}/${now.getFullYear()}`
 
-if (!userId) {
+  var now = new Date();
+  User.DateComeToWeb = `${now.getDate()}/${now.getMonth() + 1}/${now.getFullYear()}`
+
+  if (!userId) {
     userId = "user_" + Date.now() + "_" + Math.floor(Math.random() * 1000);
-    setCookie("userid", userId, 365);
+    // setCookie("userid", userId, 365);
     await addData(User)
-} 
-    //UpdateData()
+  }
+  //UpdateData()
+
+
+  // check User pause on div 
+
+  const stayTime = {};     // tổng thời gian dừng (ms)
+  let enterTime = 0;
+  let currentDiv = null;
+
+  $(".listenMouse").on("mouseenter", function () {
+    currentDiv = $(this).data("info");
+    enterTime = Date.now();
+
+    if (!stayTime[currentDiv]) {  // lúc này gán div vào obj và kiểm tra logic nếu data = undefine thì  mới khởi tạo| để chạy if thì phải true cho nên !undefine = true
+      stayTime[currentDiv] = 0; // { div1 : 0 } nếu ko gán sẽ = undefine || {div1 : undefine}
+    }
+  });
+
+  $(".listenMouse").on("mouseleave", function () {
+    if (!currentDiv) return;
+
+    const duration = Date.now() - enterTime;
+    stayTime[currentDiv] += duration; // 
+
+    currentDiv = null;
+    enterTime = 0;
+  });
+
+  // Khi người dùng rời trang → tính kết quả
+
+
+  function showResult() {
+    let maxDiv = null;
+    let maxTime = 0;
+
+    for (let div in stayTime) {
+      if (stayTime[div] > maxTime) {
+        maxTime = stayTime[div];
+        maxDiv = div;
+      }
+    }
+    console.log("Div ở lâu nhất:", maxDiv);
+    console.log("Thời gian (giây):", (maxTime / 1000).toFixed(2));
+    return `Key lâu nhất: ${maxDiv}, Time: ${(maxTime / 1000).toFixed(2)}`
+  }
+
+
+  //  --------------- mai update----
+
 
   // check state
- function sendHeartbeat() {
+  function sendHeartbeat() {
+    var strTimePauseKey = showResult();
     setDoc(doc(ManagerBehaviosUser, userId), {
-        SumMouseClick: clickCount ,
-        status: "online",
-        lastSeen: serverTimestamp()
+      TimePauseKey : strTimePauseKey,
+      SumMouseClick: clickCount,
+      lastSeen: serverTimestamp()
     }, { merge: true })  // merge: true để không ghi đè toàn bộ doc
-    .catch(err => console.error("Lỗi gửi heartbeat:", err));
-}
-// Bắt đầu heartbeat
-const heartbeatInterval = setInterval(sendHeartbeat, 20000);
-sendHeartbeat(); // gửi ngay khi load trang
+      .catch(err => console.error("Lỗi gửi heartbeat:", err));
+  }
+  // Bắt đầu heartbeat
+  setInterval(sendHeartbeat, 20000);
+  sendHeartbeat(); // gửi ngay khi load trang
 
-// check User pause on div 
+  // let lastSeenCache = null;
 
-const stayTime = {};     // tổng thời gian dừng (ms)
-    let enterTime = 0;
-    let currentDiv = null;
-
-    $(".listenMouse").on("mouseenter", function () {
-        currentDiv = $(this).data("info");
-        enterTime = Date.now();
-
-        if (!stayTime[currentDiv]) {  // lúc này gán div vào obj và kiểm tra logic nếu data = undefine thì  mới khởi tạo| để chạy if thì phải true cho nên !undefine = true
-            stayTime[currentDiv] = 0; // { div1 : 0 } nếu ko gán sẽ = undefine || {div1 : undefine}
-        }
-    });
-
-    $(".listenMouse").on("mouseleave", function () {
-        if (!currentDiv) return;
-
-        const duration = Date.now() - enterTime;
-        stayTime[currentDiv] += duration; // 
-
-        currentDiv = null;
-        enterTime = 0;
-    });
-
-    // Khi người dùng rời trang → tính kết quả
- 
-
-    function showResult() {
-        let maxDiv = null;
-        let maxTime = 0;
-
-        for (let div in stayTime) {
-            if (stayTime[div] > maxTime) {
-                maxTime = stayTime[div];
-                maxDiv = div;
-            }
-        }
-        console.log("Div ở lâu nhất:", maxDiv);
-        console.log("Thời gian (giây):", (maxTime / 1000).toFixed(2));
-       return `Key lâu nhất: ${maxDiv}, Time: ${(maxTime / 1000).toFixed(2)}`
-    }
-
-    showResult();
+  // onSnapshot(doc(ManagerBehaviosUser, userId), (docSnap) => {
+  //   if (docSnap.exists()) {
+  //     lastSeenCache = docSnap.data().lastSeen;
+  //   }
+  // });
 
 
+  // let lastOnlineState = null; // null | true | false
 
-function updateUserStatusOffline() {
-    setDoc(doc(ManagerBehaviosUser, userId), {
-        TimePauseKey : showResult(),
-        status: "offline",
-        lastSeen: serverTimestamp()
-    }, { merge: true })
-    .catch(err => console.error("Lỗi cập nhật offline:", err));
-}
+  // function isUserOnline(lastSeen) {
+  //   if (!lastSeen) return false;
 
-// Khi mất mạng tạm thời
-window.addEventListener("offline", updateUserStatusOffline);
-
-// Khi đóng tab / reload
-window.addEventListener("beforeunload", updateUserStatusOffline);
+  //   const diff = Date.now() - lastSeen.toMillis();
+  //   return diff < 30000; // 30s
+  // }
 
 });
